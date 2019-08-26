930c900cd6a5a5d76f02159b6c3a56d28d7a4452
Kelum Sampath <KelumS@99x.lk>
2019-08-26 14:20:24 +0530

    form

--- src/components/AddComplain.js
+++ src/components/AddComplain.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
+
 class AddComplain extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {name: ''};
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.name);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        
-        <h1>Hellopoooo, </h1>
-        <p>
-          Add complain
-        </p>
-        <Button type="button" class="btn btn-danger">Danger</Button>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+        </label><br></br>
+        <Button type="submit" value="Submit" >Submit</Button>
+      </form>
 
       </div>
 
