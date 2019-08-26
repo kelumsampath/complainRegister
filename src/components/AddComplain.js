d1408658758d84b8680b8c1a78d1958ab2e8eb16
Kelum Sampath <KelumS@99x.lk>
2019-08-26 14:38:00 +0530

    add complain form

--- src/components/AddComplain.js
+++ src/components/AddComplain.js
@@ -6,31 +6,56 @@ class AddComplain extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {name: ''};
+    this.state = {
+      name: '',
+      houseNo: '',
+      complain: ''
+    };
 
     this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeHouseNo = this.handleChangeHouseNo.bind(this);
+    this.handleChangeComplain = this.handleChangeComplain.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChangeName(event) {
-    this.setState({name: event.target.value});
+    this.setState({ name: event.target.value });
   }
-
+  handleChangeHouseNo(event) {
+    this.setState({ houseNo: event.target.value });
+  }
+  handleChangeComplain(event) {
+    this.setState({ complain: event.target.value });
+  }
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.name);
+    var complainData = {
+      name: this.state.name,
+      house: this.state.houseNo,
+      complain: this.state.complain
+    }
+    //alert('A name was submitted: ' + this.complainData.name);
     event.preventDefault();
+    console.log(complainData);
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
+          <label>
+            Name of complainer:
           <input type="text" value={this.state.name} onChange={this.handleChangeName} />
-        </label><br></br>
-        <Button type="submit" value="Submit" >Submit</Button>
-      </form>
+          </label><br></br>
+          <label>
+            Residencial Number:
+          <input type="Number" value={this.state.houseNo} onChange={this.handleChangeHouseNo} />
+          </label><br></br>
+          <label>
+            Complain:
+          <input type="text" value={this.state.complain} onChange={this.handleChangeComplain} />
+          </label><br></br>
+          <Button type="submit" value="Submit" >Submit</Button>
+        </form>
 
       </div>
 
